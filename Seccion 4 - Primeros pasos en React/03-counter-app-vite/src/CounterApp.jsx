0f36f93e880a70272357f7671e5bffb518ae795c
Edder Cid <yeyo.foster@gmail.com>
2024-03-03 04:09:08 -0600

    V47 - handleSubtract y handleReset

--- Seccion 4 - Primeros pasos en React/03-counter-app-vite/src/CounterApp.jsx
+++ Seccion 4 - Primeros pasos en React/03-counter-app-vite/src/CounterApp.jsx
@@ -5,15 +5,17 @@ export const CounterApp = ({ value }) => {
 
     const [counter, setCounter] = useState(value);
 
-    const handleAdd = () => {
-        setCounter(counter + 1);
-    };
+    const handleAdd = () => setCounter(counter + 1);
+    const handleSubtract = () => setCounter(counter - 1);
+    const handleReset = () => setCounter(value);
 
     return (
         <>
             <h1>CounterApp</h1>
             <h2>{counter}</h2>
             <button onClick={handleAdd}>+1</button>
+            <button onClick={handleSubtract}>-1</button>
+            <button onClick={handleReset}>Reset</button>
         </>
     );
 };
