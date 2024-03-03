d3f885cf6de159945316538545a6191606f6884c
Edder Cid <yeyo.foster@gmail.com>
2024-03-03 02:47:27 -0600

    V40 - Colocar estilos de CSS

--- Seccion 4 - Primeros pasos en React/03-counter-app-vite/src/FirstApp.jsx
+++ Seccion 4 - Primeros pasos en React/03-counter-app-vite/src/FirstApp.jsx
@@ -1,11 +1,7 @@
-const getResult = (a, b) => {
-    return a + b;
-};
-
 export const FirstApp = () => {
     return (
         <>
-            <h1>{getResult(5, 10)}</h1>
+            <h1>First App</h1>
             <p>Soy un subtitulo</p>
         </>
     )
