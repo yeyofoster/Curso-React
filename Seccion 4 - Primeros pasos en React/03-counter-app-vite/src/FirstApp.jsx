a8c9ccc2dffb78edfdabe438c7fcf75abcc3daa7
Edder Cid <yeyo.foster@gmail.com>
2024-03-03 02:39:17 -0600

    V39 - Impresion de variables en el HTML

--- Seccion 4 - Primeros pasos en React/03-counter-app-vite/src/FirstApp.jsx
+++ Seccion 4 - Primeros pasos en React/03-counter-app-vite/src/FirstApp.jsx
@@ -1,8 +1,12 @@
+const getResult = (a, b) => {
+    return a + b;
+};
+
 export const FirstApp = () => {
     return (
         <>
-            <h1>First App</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima pariatur eos maxime at deleniti iure reiciendis sed? Delectus explicabo ad dicta voluptate excepturi quidem repellat ea voluptas, amet porro sint!</p>
+            <h1>{getResult(5, 10)}</h1>
+            <p>Soy un subtitulo</p>
         </>
     )
 };
