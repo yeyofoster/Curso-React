e24c5511ac3ca002e93c2f3dbb3bca01f81e43dd
Edder Cid <yeyo.foster@gmail.com>
2024-03-03 03:25:38 -0600

    V43 - DefaultProps

--- Seccion 4 - Primeros pasos en React/03-counter-app-vite/src/FirstApp.jsx
+++ Seccion 4 - Primeros pasos en React/03-counter-app-vite/src/FirstApp.jsx
@@ -1,15 +1,22 @@
 import PropTypes from 'prop-types';
 
-export const FirstApp = ({ title, subTitle }) => {
+export const FirstApp = ({ title, subTitle, name }) => {
     return (
         <>
             <h1>{title}</h1>
             <p>{subTitle}</p>
+            <p>{name}</p>
         </>
     );
 };
 
 FirstApp.propTypes = {
-    title: PropTypes.string.isRequired,
-    subTitle: PropTypes.number
+    subTitle: PropTypes.number,
+    title: PropTypes.string.isRequired
+};
+
+FirstApp.defaultProps = {
+    name: 'Yeyo Foster',
+    subTitle: 0,
+    title: 'No hay titulo'
 };
--- Seccion 4 - Primeros pasos en React/03-counter-app-vite/src/main.jsx
+++ Seccion 4 - Primeros pasos en React/03-counter-app-vite/src/main.jsx
@@ -6,6 +6,6 @@ import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp title='Hola, soy Yeyo' />
+        <FirstApp />
     </React.StrictMode>
 );
