d73de300bf11874aa6551eac4367f77ae7d6065e
Edder Cid <yeyo.foster@gmail.com>
2024-03-03 04:00:11 -0600

    V46 - useState - Hook

--- Seccion 4 - Primeros pasos en React/03-counter-app-vite/src/CounterApp.jsx
+++ Seccion 4 - Primeros pasos en React/03-counter-app-vite/src/CounterApp.jsx
@@ -1,15 +1,18 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 export const CounterApp = ({ value }) => {
 
+    const [counter, setCounter] = useState(value);
+
     const handleAdd = () => {
-        console.log('+1');
+        setCounter(counter + 1);
     };
 
     return (
         <>
             <h1>CounterApp</h1>
-            <h2>{value}</h2>
+            <h2>{counter}</h2>
             <button onClick={handleAdd}>+1</button>
         </>
     );
@@ -17,4 +20,4 @@ export const CounterApp = ({ value }) => {
 
 CounterApp.propTypes = {
     value: PropTypes.number.isRequired
-}
+};
