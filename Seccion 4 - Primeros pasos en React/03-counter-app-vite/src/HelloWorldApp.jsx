948fc86b6348a3a18aabb30f3f1c069fb0db369a
Edder Cid <yeyo.foster@gmail.com>
2024-03-03 02:07:25 -0600

    V37 - Nuestro primer componente

---
+++ Seccion 4 - Primeros pasos en React/03-counter-app-vite/src/HelloWorldApp.jsx
@@ -0,0 +1,13 @@
+// function App() {
+//     return (<h1>Hola mundo!!!</h1>);
+// };
+
+// export default App;
+
+import React from 'react'
+
+export const HelloWorldApp = () => {
+    return (
+        <h1>Hello World App</h1>
+    )
+};
