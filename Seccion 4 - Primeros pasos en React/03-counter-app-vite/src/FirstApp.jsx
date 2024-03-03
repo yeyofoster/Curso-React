39b696105c3e0680bc07e95dbf769f7266f82335
Edder Cid <yeyo.foster@gmail.com>
2024-03-03 02:24:51 -0600

    V38 - Retornar elementos en el Componente - Fragment

--- Seccion 4 - Primeros pasos en React/03-counter-app-vite/src/FirstApp.jsx
+++ Seccion 4 - Primeros pasos en React/03-counter-app-vite/src/FirstApp.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-
 export const FirstApp = () => {
-  return (
-    <h1>First App</h1>
-  )
+    return (
+        <>
+            <h1>First App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima pariatur eos maxime at deleniti iure reiciendis sed? Delectus explicabo ad dicta voluptate excepturi quidem repellat ea voluptas, amet porro sint!</p>
+        </>
+    )
 };
