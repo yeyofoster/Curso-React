271077b92c9a576f26facd509c4422ec9e0b74e4
Edder Cid <yeyo.foster@gmail.com>
2024-03-03 02:17:16 -0600

    V37 - Tarea - Crear un nuevo componente

--- Seccion 4 - Primeros pasos en React/03-counter-app-vite/src/main.jsx
+++ Seccion 4 - Primeros pasos en React/03-counter-app-vite/src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HelloWorldApp } from './HelloWorldApp';
+// import { HelloWorldApp } from './HelloWorldApp';
+import { FirstApp } from './FirstApp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <HelloWorldApp />
+        <FirstApp />
     </React.StrictMode>
 );
