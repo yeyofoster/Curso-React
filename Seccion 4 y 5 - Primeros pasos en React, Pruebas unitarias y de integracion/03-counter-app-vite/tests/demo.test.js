d7029feb1b644f66df282542bc067419183d54f6
Edder Cid <yeyo.foster@gmail.com>
2024-03-06 02:39:07 -0600

    V54 - Jest - Expect - toBe

--- Seccion 4 y 5 - Primeros pasos en React, Pruebas unitarias y de integracion/03-counter-app-vite/tests/demo.test.js
+++ Seccion 4 y 5 - Primeros pasos en React, Pruebas unitarias y de integracion/03-counter-app-vite/tests/demo.test.js
@@ -1,5 +1,12 @@
-test('Esta prueba no debe fallar', () => {
-     if (0 === 1) {
-        throw new Error('No se puede dividir entre cero');
-     }
+describe('Pruebas en <DemoComponent />', () => {
+    test('Esta prueba no debe fallar', () => {
+        // 1. Inicialización.
+        const message1 = 'Hola mundo';
+
+        // 2. Estímulo
+        const message2 = message1.trim();
+
+        // 3. Observar el comportamiento.
+        expect(message1).toBe(message2);
+    });
 });
