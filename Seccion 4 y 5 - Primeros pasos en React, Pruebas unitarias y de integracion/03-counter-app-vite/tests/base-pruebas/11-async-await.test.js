115ac3e0ed011a257fd47eff18f9cd1bb0fd30f4
Edder Cid <yeyo.foster@gmail.com>
2024-03-14 03:07:41 -0600

    V61 - Pruebas con async-await

---
+++ Seccion 4 y 5 - Primeros pasos en React, Pruebas unitarias y de integracion/03-counter-app-vite/tests/base-pruebas/11-async-await.test.js
@@ -0,0 +1,8 @@
+import { getImagen } from "../../src/base-pruebas/11-async-await";
+
+describe('Pruebas en 11-async-await', () => {
+    test('getImagen debe de retornar un URL de la imagen', async () => {
+        const url = await getImagen();
+        expect(typeof url).toBe('string');
+    });
+});
