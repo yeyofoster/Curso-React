1eebda3a08e0aa81dd4a4e5c37fafb15f0143ef1
Edder Cid <yeyo.foster@gmail.com>
2024-03-15 00:37:42 -0600

    V65 -  Probar FirstApp

--- Seccion 4 y 5 - Primeros pasos en React, Pruebas unitarias y de integracion/03-counter-app-vite/tests/FirstApp.test.jsx
+++ Seccion 4 y 5 - Primeros pasos en React, Pruebas unitarias y de integracion/03-counter-app-vite/tests/FirstApp.test.jsx
@@ -2,8 +2,22 @@ import { render } from "@testing-library/react";
 import { FirstApp } from "../src/FirstApp";
 
 describe('Pruebas en FirstApp', () => {
+
     test('Debe de hacer match con el snapshot', () => {
         const title = 'Hola soy Yeyo';
-        render(<FirstApp title={title} />);
+        const { container } = render(<FirstApp title={title} />);
+        expect(container).toMatchSnapshot();
     });
+
+    test('Debe de mostrar el titulo en un h1', () => {
+        const title = 'Hola soy Yeyo';
+        const { container, getByText } = render(<FirstApp title={title} />);
+
+        expect(getByText(title)).toBeTruthy();
+
+        const h1 = container.querySelector('h1');
+
+        // expect(h1.innerHTML).toBe(title);
+        expect(h1.innerHTML).toContain(title);
+    })
 });
