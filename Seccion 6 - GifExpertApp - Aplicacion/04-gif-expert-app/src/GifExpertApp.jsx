18d6e4e784be3c3b7c74ea6a1b67e13c6469ca1e
Edder Cid <yeyo.foster@gmail.com>
2024-03-16 04:00:39 -0600

    V82 - GifGrid - Nuevo componente

--- Seccion 6 - GifExpertApp - Aplicacion/04-gif-expert-app/src/GifExpertApp.jsx
+++ Seccion 6 - GifExpertApp - Aplicacion/04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { AddCategory } from './components/AddCategory';
+import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
     const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
@@ -12,26 +13,19 @@ export const GifExpertApp = () => {
 
     return (
         <>
-            {/* Titulo */}
             <h1>GifExpertApp</h1>
-
             <AddCategory
-                // setCategories={setCategories}
                 onNewCategory={onAddCategory}
             />
 
-            <button onClick={onAddCategory}>Agregar</button>
-            {/* Input */}
-            <ol>
-                {
-                    categories.map(category =>
-                        <li key={category}> {category} </li>
-                    )
-                }
-            </ol>
-
-            {/* Listado de Gif */}
-            {/* Gif Item */}
+            {
+                categories.map(category =>
+                    <GifGrid
+                        key={category}
+                        category={category}
+                    />
+                )
+            }
 
         </>
     );
