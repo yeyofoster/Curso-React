20aec66b9674ea63bacf42fc6023b0aa743eb36b
Edder Cid <yeyo.foster@gmail.com>
2024-03-16 01:52:04 -0600

    V75 - GifExpertApp - Component

--- Seccion 6 - GifExpertApp - Aplicacion/04-gif-expert-app/src/GifExpertApp.jsx
+++ Seccion 6 - GifExpertApp - Aplicacion/04-gif-expert-app/src/GifExpertApp.jsx
@@ -3,7 +3,14 @@ import React from 'react'
 export const GifExpertApp = () => {
     return (
         <>
+            {/* Titulo */}
             <h1>GifExpertApp</h1>
+
+            {/* Input */}
+
+            {/* Listado de Gif */}
+            {/* Gif Item */}
+
         </>
     );
 };
