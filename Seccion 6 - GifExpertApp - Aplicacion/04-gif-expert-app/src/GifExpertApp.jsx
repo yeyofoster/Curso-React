4ab1e8e874c1155cfcbe92c103f98681d1c27484
Edder Cid <yeyo.foster@gmail.com>
2024-03-16 02:47:06 -0600

    V77 - Agregar una nueva categoría

--- Seccion 6 - GifExpertApp - Aplicacion/04-gif-expert-app/src/GifExpertApp.jsx
+++ Seccion 6 - GifExpertApp - Aplicacion/04-gif-expert-app/src/GifExpertApp.jsx
@@ -3,16 +3,23 @@ import React, { useState } from 'react'
 export const GifExpertApp = () => {
     const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
 
+    const onAddCategory = () => {
+        setCategories([...categories, 'Yeyo Foster']);
+    };
+
     return (
         <>
             {/* Titulo */}
             <h1>GifExpertApp</h1>
 
+            <button onClick={onAddCategory}>Agregar</button>
             {/* Input */}
             <ol>
-                {categories.map(category => {
-                    return <li key={category}> {category} </li>;
-                })}
+                {
+                    categories.map(category =>
+                        <li key={category}> {category} </li>
+                    )
+                }
             </ol>
 
             {/* Listado de Gif */}
