d0fcc2a474b99c4b4a95894e0dcb6ebc73170e42
Edder Cid <yeyo.foster@gmail.com>
2024-03-16 19:49:14 -0600

    V86 - Mostrar los títulos de las imágenes

--- Seccion 6 - GifExpertApp - Aplicacion/04-gif-expert-app/src/components/GifGrid.jsx
+++ Seccion 6 - GifExpertApp - Aplicacion/04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,15 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ category }) => {
 
+    const [images, setImages] = useState([]);
+
+    const getImages = async () => {
+        const newImages = await getGifs(category);
+        setImages(newImages);
+    }
+
     useEffect(() => {
-        getGifs(category);
+        getImages();
     }, []);
 
     return (
         <>
             <h3>{category}</h3>
+
+            <ol>
+                {
+                    images.map(image =>
+                        <li key={image.id}>
+                            {image.title}
+                        </li>
+                    )
+                }
+            </ol>
         </>
     );
 };
