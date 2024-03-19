751765a8f28964a7134bd14c567a39caa035c51e
Edder Cid <yeyo.foster@gmail.com>
2024-03-19 00:18:38 -0600

    V104 - Pruebas en el componente - GifGridItem

--- Seccion 6 - GifExpertApp - Aplicacion/04-gif-expert-app/tests/components/GifItem.test.jsx
+++ Seccion 6 - GifExpertApp - Aplicacion/04-gif-expert-app/tests/components/GifItem.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifItem } from "../../src/components/GifItem";
 
 describe('Pruebas en <GifItem />', () => {
@@ -12,5 +12,21 @@ describe('Pruebas en <GifItem />', () => {
         );
 
         expect(container).toMatchSnapshot;
+    });
+
+    test('Debe de mostrar la imagen con el URL y ALT indicado', () => {
+        render(<GifItem title={title} url={url} />);
+
+        // expect(screen.getByRole('img').src).toBe(url);
+        // expect(screen.getByRole('img').alt).toBe(title);
+
+        const { src, alt } = screen.getByRole('img');
+        expect(src).toBe(url);
+        expect(alt).toBe(title);
+    });
+
+    test('Debe de mostrar el titulo en el componente', () => {
+        render(<GifItem title={title} url={url} />);
+        expect(screen.getByText(title)).toBeTruthy();
     });
 });
