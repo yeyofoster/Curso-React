63ff80364ae89d1ada7e74b7ef9d2827bbf462e1
Edder Cid <yeyo.foster@gmail.com>
2024-03-19 03:36:13 -0600

    V119 - Exponer métodos del Hook

--- Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/01-useState/CounterWithCustomHook.jsx
+++ Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/01-useState/CounterWithCustomHook.jsx
@@ -1,17 +1,17 @@
 import { useCounter } from "../hooks/useCounter";
 
 export const CounterWithCustomHook = () => {
-
-    const { counter } = useCounter(0);
+    const { counter, increment, decrement, reset } = useCounter(5);
+    const step = 5;
 
     return (
         <>
             <h1>Counter with Hook: {counter}</h1>
             <hr />
 
-            <button className="btn btn-primary">+1</button>
-            <button className="btn btn-secondary">Reset</button>
-            <button className="btn btn-danger">-1</button>
+            <button className="btn btn-primary" onClick={() => increment(step)}>+1</button>
+            <button className="btn btn-secondary" onClick={reset}>Reset</button>
+            <button className="btn btn-danger" onClick={() => decrement(step)}>-1</button>
         </>
     );
 };
--- Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/hooks/useCounter.js
+++ Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/hooks/useCounter.js
@@ -1,9 +1,26 @@
 import { useState } from "react";
 
-export const useCounter = (initialValue = 10) => {
+export const useCounter = (initialValue = 10, minValue = 0) => {
     const [counter, setCounter] = useState(initialValue);
+
+    const increment = (value) => {
+        setCounter(counter + value);
+    };
+
+    const decrement = (value) => {
+        if (counter <= minValue)
+            return;
+        setCounter(counter - value);
+    };
+
+    const reset = () => {
+        setCounter(initialValue);
+    };
 
     return {
         counter,
+        increment,
+        decrement,
+        reset
     };
 };
