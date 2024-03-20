0bd9fb3e2aef087bbb8c581791be8b103ff76ad9
Edder Cid <yeyo.foster@gmail.com>
2024-03-20 03:14:46 -0600

    V120 - useEffect - SimpleForm

---
+++ Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+export const SimpleForm = () => {
+    const [formState, setFormState] = useState({
+        username: 'Roberto',
+        email: 'rcid@gmail.com'
+    });
+
+    const { username, email } = formState;
+
+    const onInputChange = ({ target }) => {
+        const { name, value } = target;
+        setFormState({ ...formState, [name]: value });
+    };
+
+    useEffect(() => {
+        console.log('UseEffect');
+    }, []);
+
+
+    return (
+        <>
+            <h1>Formulario Simple</h1>
+            <hr />
+
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={onInputChange} />
+
+            <input
+                type="email"
+                className="form-control mt-3"
+                placeholder="yeyo.foster@gmail.com"
+                name="email"
+                value={email}
+                onChange={onInputChange} />
+        </>
+    );
+};
