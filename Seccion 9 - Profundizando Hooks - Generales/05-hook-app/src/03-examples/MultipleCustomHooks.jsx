f4ea4828beb0d56266623fa066f39bb183f5953b
Edder Cid <yeyo.foster@gmail.com>
2024-03-21 03:17:05 -0600

    V127 - Parametrizar y consumir nuestro Custom Hook

--- Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/03-examples/MultipleCustomHooks.jsx
+++ Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -1,14 +1,19 @@
 import { useFetch } from "../hooks";
 
 export const MultipleCustomHooks = () => {
-    const url = 'https://pokeapi.co/api/v2/pokemon/ditto';
+    const url = 'https://pokeapi.co/api/v2/pokemon/1';
 
-    useFetch(url);
+    const { data, hasError, isLoading } = useFetch(url);
 
     return (
         <>
-            <h1>MultipleCustomHooks</h1>
+            <h1>Información de Pokemon</h1>
             <hr />
+            {isLoading ?
+                <h3>Cargando...</h3> :
+                <h3>{data?.name}</h3>
+            }
+
         </>
     );
 };
--- Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/hooks/useFetch.js
+++ Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/hooks/useFetch.js
@@ -9,17 +9,51 @@ export const useFetch = (url) => {
     });
 
     useEffect(() => {
+        isLoadingState();
         getFetch();
 
         return () => {
 
         }
-    }, []);
+    }, [url]);
+
+    const isLoadingState = () => {
+        setState({
+            data: null,
+            isLoading: true,
+            hasError: false,
+            error: null
+        });
+    };
 
     const getFetch = async () => {
         const response = await fetch(url);
+
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        if (!response.ok) {
+            setState({
+                data: null,
+                isLoading: false,
+                hasError: true,
+                error: {
+                    code: response.status,
+                    message: response.statusText
+                }
+            });
+            return;
+        }
+
         const data = await response.json();
         console.log(data);
+        setState({
+            data: data,
+            isLoading: false,
+            hasError: false,
+            error: null
+        });
+
+        // Manejo de cache
     };
 
     return {
