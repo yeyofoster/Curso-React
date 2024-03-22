51cd61b7d99112bbb747a8cbdc3a58b91e87f2da
Edder Cid <yeyo.foster@gmail.com>
2024-03-22 01:32:34 -0600

    V136 - Tarea Memorize

---
+++ Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/07-tarea-memo/07-tarea-memo/Padre.jsx
@@ -0,0 +1,33 @@
+import { Hijo } from './Hijo'
+import { useCallback, useState } from 'react';
+
+export const Padre = () => {
+
+    const numbers = [2, 4, 6, 8, 10];
+    const [valor, setValor] = useState(0);
+
+    const incrementar = useCallback(
+        (num) => { setValor((oldValue) => oldValue + num) },
+        [],
+    );
+
+    return (
+        <div>
+            <h1>Padre</h1>
+            <p> Total: {valor} </p>
+
+            <hr />
+
+            {
+                numbers.map(n => (
+                    <Hijo
+                        key={n}
+                        numero={n}
+                        incrementar={incrementar}
+                    />
+                ))
+            }
+            {/* <Hijo /> */}
+        </div>
+    );
+};
--- Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/main.jsx
+++ Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/main.jsx
@@ -10,12 +10,13 @@ import ReactDOM from 'react-dom/client';
 // import { Layout } from './05-useLayoutEffect/Layout';
 // import { Memorize } from './06-memos/Memorize';
 // import { MemoHook } from './06-memos/MemoHook';
-import { CallbackHook } from './06-memos/CallbackHook';
+// import { CallbackHook } from './06-memos/CallbackHook';
+import { Padre } from './07-tarea-memo/07-tarea-memo/Padre';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <CallbackHook />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Padre />
+  // </React.StrictMode>
 );
