1eeed67758a76cb4fb653f25a024bf3d2cd0a8b8
Edder Cid <yeyo.foster@gmail.com>
2024-03-22 00:55:00 -0600

    V134 - useCallback

--- Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/main.jsx
+++ Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/main.jsx
@@ -9,12 +9,13 @@ import ReactDOM from 'react-dom/client';
 // import { FocusScreen } from './04-useRef/FocusScreen';
 // import { Layout } from './05-useLayoutEffect/Layout';
 // import { Memorize } from './06-memos/Memorize';
-import { MemoHook } from './06-memos/MemoHook';
+// import { MemoHook } from './06-memos/MemoHook';
+import { CallbackHook } from './06-memos/CallbackHook';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MemoHook />
+    <CallbackHook />
   </React.StrictMode>
 );
