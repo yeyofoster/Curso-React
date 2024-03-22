75847b5e7cd73f06b81dd6b5695412be2d1990c8
Edder Cid <yeyo.foster@gmail.com>
2024-03-21 23:58:39 -0600

    V132 - Memo - Método de React

---
+++ Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/06-memos/Small.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Small = React.memo(({ value }) => {
+    console.log('Me volvi a dibujar');
+    return (
+        <small> {value} </small>
+    );
+});
--- Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/main.jsx
+++ Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/main.jsx
@@ -7,12 +7,13 @@ import ReactDOM from 'react-dom/client';
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 // import { FocusScreen } from './04-useRef/FocusScreen';
-import { Layout } from './05-useLayoutEffect/Layout';
+// import { Layout } from './05-useLayoutEffect/Layout';
+import { Memorize } from './06-memos/Memorize';
 
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Layout />
+    <Memorize />
   </React.StrictMode>
 );
