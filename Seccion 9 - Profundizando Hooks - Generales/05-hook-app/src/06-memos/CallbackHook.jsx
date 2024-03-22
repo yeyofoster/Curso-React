19b54e1629b60c94c3f8ead9edb5e6df719d2b47
Edder Cid <yeyo.foster@gmail.com>
2024-03-22 00:59:05 -0600

    V135 - useCallback con argumentos

--- Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/06-memos/CallbackHook.jsx
+++ Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/06-memos/CallbackHook.jsx
@@ -5,7 +5,7 @@ export const CallbackHook = () => {
     const [counter, setCounter] = useState(10);
 
     const incrementFather = useCallback(
-        () => { setCounter((value) => value + 1) },
+        (value) => { setCounter((counter) => counter + value) },
         []
     );
 
--- Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/06-memos/ShowIncrement.jsx
+++ Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/06-memos/ShowIncrement.jsx
@@ -6,7 +6,7 @@ export const ShowIncrement = React.memo(({ increment }) => {
     return (
         <button
             className="btn btn-primary"
-            onClick={() => increment()}
+            onClick={() => increment(5)}
         >
             Incrementar
         </button>
