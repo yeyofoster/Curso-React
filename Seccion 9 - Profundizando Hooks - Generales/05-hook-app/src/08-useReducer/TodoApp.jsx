2991ff9c26d071e08a1f72fe82b8832703f915f8
Edder Cid <yeyo.foster@gmail.com>
2024-03-24 03:13:19 -0600

    V147 - Agregar un nuevo TODO

--- Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/08-useReducer/TodoApp.jsx
+++ Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -22,6 +22,12 @@ export const TodoApp = () => {
 
     const handleNewTodo = (todo) => {
         console.log({ todo });
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: todo
+        };
+
+        dispatch(action);
     };
 
     return (
