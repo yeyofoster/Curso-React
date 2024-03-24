a98023498df630f5e46d2e092eeabadfd878d9fa
Edder Cid <yeyo.foster@gmail.com>
2024-03-24 02:15:32 -0600

    V144 - Creando el cascaron de la lista TODOs

--- Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/08-useReducer/TodoApp.jsx
+++ Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/08-useReducer/TodoApp.jsx
@@ -20,14 +20,42 @@ export const TodoApp = () => {
 
     return (
         <>
-            <h1>TodoApp</h1>
+            <h1>TodoApp: 10 - <small>Pendientes: 2</small></h1>
             <hr />
 
-            <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-            </ul>
+            <div className="row">
+                <div className="col-7">
+                    <ul className="list-group">
+                        {
+                            todos.map((todo, index) => (
+                                <li key={todo.id} className="list-group-item d-flex justify-content-between">
+                                    <span className="align-self-center">Item {index + 1}</span>
+                                    <button className="btn btn-danger">
+                                        Borrar
+                                    </button>
+                                </li>
+                            ))
+                        }
+                    </ul>
+                </div>
+                <div className="col-5">
+                    <h4>Agregar TODO</h4>
+                    <hr />
+                    <form action="">
+                        <input
+                            type="text"
+                            placeholder="Que hay que hacer?"
+                            className="form-control"
+                        />
+
+                        <button
+                            type="submit"
+                            className="btn btn-primary mt-3">
+                            Agregar
+                        </button>
+                    </form>
+                </div>
+            </div>
         </>
     );
 };
