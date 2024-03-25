139732a8e5cad84c2e907b83bac6ba6a0d5b7e3b
Edder Cid <yeyo.foster@gmail.com>
2024-03-25 02:31:02 -0600

    V158 - Configurar Router con React

--- Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/09-useContext/MainApp.jsx
+++ Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/09-useContext/MainApp.jsx
@@ -1,8 +1,22 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { HomePage } from "./HomePage";
+import { AboutPage } from "./AboutPage";
+import { LoginPage } from "./LoginPage";
+
 export const MainApp = () => {
     return (
         <>
             <h1>Main App</h1>
             <hr />
+
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="login" element={<LoginPage />} />
+                <Route path="about" element={<AboutPage />} />
+
+                {/* <Route path="/*" element={<LoginPage />} /> */}
+                <Route path="/*" element={<Navigate to='/about' />} />
+            </Routes>
         </>
     );
 };
