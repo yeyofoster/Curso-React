26e433c600766562a64da173ead09b9629b3f69d
Edder Cid <yeyo.foster@gmail.com>
2024-03-25 03:29:59 -0600

    V159 - Link

--- Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/09-useContext/MainApp.jsx
+++ Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/09-useContext/MainApp.jsx
@@ -2,12 +2,15 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { HomePage } from "./HomePage";
 import { AboutPage } from "./AboutPage";
 import { LoginPage } from "./LoginPage";
+import { Navbar } from "./Navbar";
 
 export const MainApp = () => {
     return (
         <>
             <h1>Main App</h1>
             <hr />
+
+            <Navbar />
 
             <Routes>
                 <Route path="/" element={<HomePage />} />
