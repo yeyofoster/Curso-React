25252077ccb5bf936c7abb3e5b8e45c6dc279c5c
Edder Cid <yeyo.foster@gmail.com>
2024-03-26 00:39:53 -0600

    V162 - useContext

--- Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/09-useContext/HomePage.jsx
+++ Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/09-useContext/HomePage.jsx
@@ -1,8 +1,29 @@
+import { useContext } from "react";
+import { UserContext } from "./context/UserContext";
+
 export const HomePage = () => {
+    const { user, setUser } = useContext(UserContext);
     return (
         <>
-            <h1>Home Page</h1>
+            <h1>Home Page: {user?.name}</h1>
             <hr />
+
+            <pre>
+                {JSON.stringify(user, null, 3)}
+            </pre>
+
+            <button
+                className="btn btn-primary"
+                onClick={
+                    () => setUser({
+                        id: 123,
+                        name: 'Yeyo Foster',
+                        email: 'yeyo.foster@gmail.com'
+                    })
+                }
+            >
+                Establecer usuario
+            </button>
         </>
     );
 };
--- Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/09-useContext/context/UserProvider.jsx
+++ Seccion 9 - Profundizando Hooks - Generales/05-hook-app/src/09-useContext/context/UserProvider.jsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import { UserContext } from "./UserContext";
 
-const user = {
-    id: 123,
-    name: 'Yeyo Foster',
-    email: 'yeyo.foster@gmail.com'
-};
+// const user = {
+//     id: 123,
+//     name: 'Yeyo Foster',
+//     email: 'yeyo.foster@gmail.com'
+// };
 
 export const UserProvider = ({ children }) => {
+    const [user, setUser] = useState();
+    
     return (
-        <UserContext.Provider value={{ user }}>
+        <UserContext.Provider value={{ user, setUser }}>
             {children}
         </UserContext.Provider>
     );
