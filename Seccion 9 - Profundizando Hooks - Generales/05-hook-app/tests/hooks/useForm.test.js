acc5060947dc7c22f0f770e9dc410b8154b19d67
Edder Cid <yeyo.foster@gmail.com>
2024-03-28 23:58:46 -0600

    V169 - Pruebas sobre useForm - CustomHook

---
+++ Seccion 9 - Profundizando Hooks - Generales/05-hook-app/tests/hooks/useForm.test.js
@@ -0,0 +1,45 @@
+import { act, renderHook } from "@testing-library/react";
+import { useForm } from "../../src/hooks/useForm";
+
+describe('Pruebas en useForm', () => {
+    const initialForm = {
+        name: 'Yeyo',
+        email: 'yeyo@gmail.com'
+    };
+
+    test('Debe de regresar los valores por defecto', () => {
+        const { result } = renderHook(() => useForm(initialForm));
+        expect(result.current).toEqual({
+            name: initialForm.name,
+            email: initialForm.email,
+            formState: initialForm,
+            onInputChange: expect.any(Function),
+            onResetForm: expect.any(Function)
+        });
+    });
+
+    test('Debe de cambiar el nombre del formulario', () => {
+        const newValue = 'Foster';
+        const { result } = renderHook(() => useForm(initialForm));
+        const { onInputChange } = result.current;
+        act(() => {
+            const event = { target: { name: 'name', value: newValue } }
+            onInputChange(event);
+        });
+        expect(result.current.name).toBe(newValue);
+        expect(result.current.formState.name).toBe(newValue);
+    });
+
+    test('Debe de realizar el reset del formulario', () => {
+        const newValue = 'Foster';
+        const { result } = renderHook(() => useForm(initialForm));
+        const { onInputChange, onResetForm } = result.current;
+        act(() => {
+            const event = { target: { name: 'name', value: newValue } }
+            onInputChange(event);
+            onResetForm();
+        });
+        expect(result.current.name).toBe(initialForm.name);
+        expect(result.current.formState.name).toBe(initialForm.name);
+    });
+});
