198fa2e07cec3ea72aed2449a109cbf0d43b9f6c
Edder Cid <yeyo.foster@gmail.com>
2024-03-30 03:43:26 -0600

    V175 - Pruebas en los eventos del TodoItem

--- Seccion 9 - Profundizando Hooks - Generales/05-hook-app/tests/08-useReducer/TodoItem.test.jsx
+++ Seccion 9 - Profundizando Hooks - Generales/05-hook-app/tests/08-useReducer/TodoItem.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { TodoItem } from "../../src/08-useReducer/TodoItem";
 
 
@@ -29,5 +29,46 @@ describe('Pruebas en <TodoItem />', () => {
         const spanElement = screen.getByLabelText('span');
         expect(spanElement.className).toContain('align-self-center');
         expect(spanElement.className).not.toContain('text-decoration-line-through');
-    })
+    });
+
+    test('Debe mostrar el Todo completado', () => {
+        todo.done = true;
+        render(
+            <TodoItem
+                todo={todo}
+                onDeleteTodo={onDeleteTodo}
+                onToggleTodo={onToggleTodo}
+            />
+        );
+        const spanElement = screen.getByLabelText('span');
+        expect(spanElement.className).toContain('text-decoration-line-through');
+    });
+
+    test('span debe de llamar el ToggleTodo cuando se hace click', () => {
+        render(
+            <TodoItem
+                todo={todo}
+                onDeleteTodo={onDeleteTodo}
+                onToggleTodo={onToggleTodo}
+            />
+        );
+        const spanElement = screen.getByLabelText('span');
+
+        fireEvent.click(spanElement);
+        expect(onToggleTodo).toHaveBeenCalledWith(1);
+    });
+
+    test('button debe de llamar el DeleteTodo', () => {
+        render(
+            <TodoItem
+                todo={todo}
+                onDeleteTodo={onDeleteTodo}
+                onToggleTodo={onToggleTodo}
+            />
+        );
+        const buttonElement = screen.getByRole('button');
+
+        fireEvent.click(buttonElement);
+        expect(onDeleteTodo).toHaveBeenCalledWith(1);
+    });
 });
