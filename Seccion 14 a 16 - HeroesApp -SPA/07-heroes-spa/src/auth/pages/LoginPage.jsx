c3942362d9578d3b78bd9aa02c5cd1de30cfe3b2
Edder Cid <yeyo.foster@gmail.com>
2024-04-09 02:56:13 -0600

    V194 - Navigate push / replace - useNavigate

--- Seccion 14 a 16 - HeroesApp -SPA/07-heroes-spa/src/auth/pages/LoginPage.jsx
+++ Seccion 14 a 16 - HeroesApp -SPA/07-heroes-spa/src/auth/pages/LoginPage.jsx
@@ -1,5 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+
 export const LoginPage = () => {
+
+    const navigator = useNavigate();
+
+    const onLogin = () => {
+        navigator('/', {
+            replace: true
+        });
+    };
+
     return (
-        <h1>Login Page</h1>
+        <div className="container mt-5">
+            <h1>Login</h1>
+            <hr />
+            <button
+                className="btn btn-primary"
+                onClick={onLogin}
+            >
+                Login
+            </button>
+        </div>
     );
 };
--- Seccion 14 a 16 - HeroesApp -SPA/07-heroes-spa/src/ui/components/Navbar.jsx
+++ Seccion 14 a 16 - HeroesApp -SPA/07-heroes-spa/src/ui/components/Navbar.jsx
@@ -1,9 +1,17 @@
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 
 export const Navbar = () => {
+
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        navigate('/login', {
+            replace: true
+        });
+    };
+
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
-
             <Link
                 className="navbar-brand"
                 to="/"
@@ -49,7 +57,10 @@ export const Navbar = () => {
                     <span className='nav-item nav-link text-primary'>
                         Yeyo
                     </span>
-                    <button className='nav-item nav-link btn'>
+                    <button
+                        className='nav-item nav-link btn'
+                        onClick={onLogout}
+                    >
                         Logout
                     </button>
                 </ul>
