468b82645db4343d6bff388b88d67980c4536d1f
Edder Cid <yeyo.foster@gmail.com>
2024-04-09 01:51:28 -0600

    V192 - Colocar clase de la ruta activa

--- Seccion 14 a 16 - HeroesApp -SPA/07-heroes-spa/src/router/AppRouter.jsx
+++ Seccion 14 a 16 - HeroesApp -SPA/07-heroes-spa/src/router/AppRouter.jsx
@@ -1,15 +1,16 @@
 import { Navigate, Route, Routes } from "react-router-dom";
-import { MarvelPage } from "../heroes/pages/MarvelPage";
-import { DcPage } from "../heroes/pages/DcPAge";
-import { Login } from "../auth/pages/Login";
+import { MarvelPage, DcPage } from "../heroes";
+import { LoginPage } from "../auth";
+import { Navbar } from '../ui'
 
 export const AppRouter = () => {
     return (
         <>
+            <Navbar />
             <Routes>
                 <Route path="marvel" element={<MarvelPage />} />
                 <Route path="dc" element={<DcPage />} />
-                <Route path="login" element={<Login />} />
+                <Route path="login" element={<LoginPage />} />
                 <Route path="/" element={<Navigate to='/marvel' />} />
             </Routes>
         </>
