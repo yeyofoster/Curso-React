ff2da7cdd0ec803b649d15f7afb7fd390f26bb30
Edder Cid <yeyo.foster@gmail.com>
2024-04-11 22:05:52 -0600

    V195 - Tarjetas con la información del Heroe

--- Seccion 14 a 16 - HeroesApp -SPA/07-heroes-spa/src/heroes/components/HeroList.jsx
+++ Seccion 14 a 16 - HeroesApp -SPA/07-heroes-spa/src/heroes/components/HeroList.jsx
@@ -1,16 +1,18 @@
 import { getHeroesByPublisher } from "../helpers";
+import { HeroCard } from "./HeroCard";
 
 export const HeroList = ({ publisher }) => {
     const heroes = getHeroesByPublisher(publisher);
     return (
-        <ul>
+        <div className="row rows-cols-1 row-cols-md-3 g-3">
             {
                 heroes.map(hero =>
-                    <li key={hero.id}>
-                        {hero.superhero}
-                    </li>
+                    <HeroCard
+                        key={hero.id}
+                        {...hero}
+                    />
                 )
             }
-        </ul>
+        </div>
     );
 };
