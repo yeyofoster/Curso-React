b0ceea9afd838f65e2df4886411153e8b0467f28
Edder Cid <yeyo.foster@gmail.com>
2024-04-11 23:35:53 -0600

    V202 - Diseño de la pantalla de búsqueda

--- Seccion 14 a 16 - HeroesApp -SPA/07-heroes-spa/src/heroes/pages/SearchPage.jsx
+++ Seccion 14 a 16 - HeroesApp -SPA/07-heroes-spa/src/heroes/pages/SearchPage.jsx
@@ -1,5 +1,42 @@
+import { HeroCard } from "../components";
+
 export const SearchPage = () => {
     return (
-        <h1>SearchPage</h1>
+        <>
+            <h1>Search</h1>
+            <hr />
+
+            <div className="row">
+                <div className="col-5">
+                    <h4>Searching</h4>
+                    <form action="">
+                        <input
+                            placeholder="Search a hero"
+                            type="text"
+                            className="form-control"
+                            name="searchText"
+                            autoComplete="off"
+                        />
+                        <button className="btn btn-outline-primary mt-2">
+                            Search
+                        </button>
+                    </form>
+                </div>
+
+                <div className="col-7">
+                    <h4>Results</h4>
+                    <hr />
+                    <div className="alert alert-primary">
+                        Search a hero
+                    </div>
+
+                    <div className="alert alert-danger">
+                        No hero with <b>ABC</b>
+                    </div>
+
+                    {/* <HeroCard /> */}
+                </div>
+            </div>
+        </>
     );
 };
