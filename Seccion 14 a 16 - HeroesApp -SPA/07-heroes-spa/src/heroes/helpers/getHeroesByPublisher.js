cf02f63d9c86829b83eccc159d8fc8ad15cd2313
Edder Cid <yeyo.foster@gmail.com>
2024-04-11 22:52:16 -0600

    V198 - Leer argumentos por URL

--- Seccion 14 a 16 - HeroesApp -SPA/07-heroes-spa/src/heroes/helpers/getHeroesByPublisher.js
+++ Seccion 14 a 16 - HeroesApp -SPA/07-heroes-spa/src/heroes/helpers/getHeroesByPublisher.js
@@ -1,10 +1,10 @@
 import { heroes } from "../data/heroes";
 
 export const getHeroesByPublisher = (publisher) => {
-    const validPulishers = ['DC Comics', 'Marvel Comics'];
-    if (!validPulishers.includes(publisher)) {
+    const validPublishers = ['DC Comics', 'Marvel Comics'];
+    if (!validPublishers.includes(publisher)) {
         throw new Error(`${publisher} is not a valid publisher`);
     }
 
-    return heroes.filter(heroe => heroe.publisher === publisher);
+    return heroes.filter(hero => hero.publisher === publisher);
 };
--- Seccion 14 a 16 - HeroesApp -SPA/07-heroes-spa/src/heroes/pages/HeroPage.jsx
+++ Seccion 14 a 16 - HeroesApp -SPA/07-heroes-spa/src/heroes/pages/HeroPage.jsx
@@ -1,5 +1,16 @@
+import { Navigate, useParams } from "react-router-dom";
+import { getHeroById } from "../helpers";
+import { HeroCard } from "../components";
+
 export const HeroPage = () => {
+    const { heroId } = useParams();
+    const hero = getHeroById(heroId);
+
+    if (!hero) {
+        return <Navigate to='/' />
+    }
+
     return (
-        <h1>HeroPage</h1>
+        <HeroCard {...hero} />
     );
 };
