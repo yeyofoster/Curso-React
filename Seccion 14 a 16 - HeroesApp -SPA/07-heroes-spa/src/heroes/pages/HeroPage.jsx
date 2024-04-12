e653cf29043a111a09bacc4bc1f2834b875c164d
Edder Cid <yeyo.foster@gmail.com>
2024-04-11 23:10:41 -0600

    V199 - Estilo del componente HeroScreen

--- Seccion 14 a 16 - HeroesApp -SPA/07-heroes-spa/src/heroes/pages/HeroPage.jsx
+++ Seccion 14 a 16 - HeroesApp -SPA/07-heroes-spa/src/heroes/pages/HeroPage.jsx
@@ -1,16 +1,45 @@
-import { Navigate, useParams } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 import { getHeroById } from "../helpers";
-import { HeroCard } from "../components";
 
 export const HeroPage = () => {
     const { heroId } = useParams();
     const hero = getHeroById(heroId);
+    const navigator = useNavigate();
+    const onNavigateBack = () => {
+        navigator(-1);
+    };
 
     if (!hero) {
         return <Navigate to='/' />
     }
 
     return (
-        <HeroCard {...hero} />
+        <div className="row mt-5">
+            <div className="col-4">
+                <img
+                    alt={hero.superhero}
+                    className="img-thumbnail"
+                    src={`/assets/heroes/${heroId}.jpg`}
+                />
+            </div>
+            <div className="col-8">
+                <h3>{hero.superhero}</h3>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item"><b>Alter ego:</b> {hero.alter_ego}</li>
+                    <li className="list-group-item"><b>Publisher:</b> {hero.publisher}</li>
+                    <li className="list-group-item"><b>First appearance:</b> {hero.first_appearance}</li>
+                </ul>
+
+                <h5 className="mt-3">Characters: </h5>
+                <p>{hero.characters}</p>
+
+                <button
+                    className="btn btn-outline-primary"
+                    onClick={onNavigateBack}
+                >
+                    Regresar
+                </button>
+            </div>
+        </div>
     );
 };
