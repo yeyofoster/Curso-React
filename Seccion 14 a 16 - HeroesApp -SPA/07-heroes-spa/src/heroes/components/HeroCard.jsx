81e907a4ec2431a4d87624ea2a7a172b2e300c0a
Edder Cid <yeyo.foster@gmail.com>
2024-04-11 22:32:28 -0600

    V197 - Tarjeta de Heroe - parte 2

--- Seccion 14 a 16 - HeroesApp -SPA/07-heroes-spa/src/heroes/components/HeroCard.jsx
+++ Seccion 14 a 16 - HeroesApp -SPA/07-heroes-spa/src/heroes/components/HeroCard.jsx
@@ -1,3 +1,13 @@
+import { Link } from "react-router-dom";
+
+const CharactersByHero = ({ alter_ego, characters }) => {
+    // if ((alter_ego === characters)) return <></>;
+    // return <p>{characters}</p>;
+    return (alter_ego === characters)
+        ? <></>
+        : <p>{characters}</p>;
+};
+
 export const HeroCard = ({
     id,
     superhero,
@@ -6,9 +16,7 @@ export const HeroCard = ({
     first_appearance,
     characters
 }) => {
-
     const heroImageUrl = `/assets/heroes/${id}.jpg`;
-    console.log(heroImageUrl);
 
     return (
         <div className="col">
@@ -22,7 +30,19 @@ export const HeroCard = ({
                         <div className="card-body">
                             <h5 className="card-title">{superhero}</h5>
                             <p className="card-text">{alter_ego}</p>
-                            <p>{characters}</p>
+                            {/* {
+                                (alter_ego !== characters) &&
+                                <p>{characters}</p>
+                            } */}
+
+                            <CharactersByHero alter_ego={alter_ego} characters={characters} />
+                            <p className="card-text">
+                                <small className="text-muted">{first_appearance}</small>
+                            </p>
+
+                            <Link to={`/hero/${id}`}>
+                                Más
+                            </Link>
                         </div>
                     </div>
                 </div>
