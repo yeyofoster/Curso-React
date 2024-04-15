9b4fb407ca40712114118c3e2d743c8a624da775
Edder Cid <yeyo.foster@gmail.com>
2024-04-15 02:20:33 -0600

    V213 - Mantener el usuario activo

--- Seccion 14 a 16 - HeroesApp -SPA/07-heroes-spa/src/auth/context/AuthProvider.jsx
+++ Seccion 14 a 16 - HeroesApp -SPA/07-heroes-spa/src/auth/context/AuthProvider.jsx
@@ -4,21 +4,32 @@ import { authReducer } from "./authReducer";
 import { types } from "../types/types";
 
 export const AuthProvider = ({ children }) => {
-    const initialState = {
-        logged: false
+    // const initialState = {
+    //     logged: false
+    // };
+
+    const init = () => {
+        const user = JSON.parse(localStorage.getItem('user'));
+
+        return {
+            logged: !!user,
+            user: user
+        }
     };
 
-    const [authState, authDispatch] = useReducer(authReducer, initialState);
+    const [authState, authDispatch] = useReducer(authReducer, {}, init);
 
     const login = (name = '') => {
+        const user = {
+            id: 'Yeyo',
+            name: name
+        };
         const action = {
             type: types.login,
-            payload: {
-                id: 'Yeyo',
-                name: name
-            }
+            payload: user
         };
 
+        localStorage.setItem('user', JSON.stringify(user)); 
         authDispatch(action);
     };
 
