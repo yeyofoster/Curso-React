07614a6c19eda5b26bd3a151f838dc27ccc74c02
Edder Cid <yeyo.foster@gmail.com>
2024-04-15 02:59:35 -0600

    V216 - Rutas públicas

---
+++ Seccion 14 a 16 - HeroesApp -SPA/07-heroes-spa/src/router/PublicRoute.jsx
@@ -0,0 +1,11 @@
+import { AuthContext } from "../auth";
+import { Navigate } from "react-router-dom";
+import { useContext } from "react";
+
+export const PublicRoute = ({ children }) => {
+    const { logged } = useContext(AuthContext);
+
+    return (!logged)
+        ? children
+        : <Navigate to='/marvel' />;
+};
