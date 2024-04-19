657a8750076238cdeac2e69aa0d49b55751feed5
Edder Cid <yeyo.foster@gmail.com>
2024-04-19 02:25:19 -0600

    V244 - LoginPage - Diseño - Segunda parte

--- Seccion 17 - JournalApp - MaterialUI/08-journal-app/src/auth/pages/LoginPage.jsx
+++ Seccion 17 - JournalApp - MaterialUI/08-journal-app/src/auth/pages/LoginPage.jsx
@@ -1,4 +1,6 @@
-import { Grid, TextField, Typography } from "@mui/material"
+import { Button, Grid, Link, TextField, Typography } from "@mui/material"
+import { Google } from "@mui/icons-material";
+import { Link as RouterLink } from "react-router-dom";
 
 export const LoginPage = () => {
     return (
@@ -38,6 +40,41 @@ export const LoginPage = () => {
                                 fullWidth
                             />
                         </Grid>
+                    </Grid>
+
+                    <Grid
+                        container
+                        spacing={2}
+                        sx={{ mb: 2, mt: 1 }}>
+                        <Grid item xs={12} sm={6}>
+                            <Button
+                                variant="contained"
+                                fullWidth>
+                                <Typography>
+                                    Login
+                                </Typography>
+                            </Button>
+                        </Grid>
+
+                        <Grid item xs={12} sm={6}>
+                            <Button
+                                variant="contained"
+                                fullWidth>
+                                <Google />
+                                <Typography sx={{ ml: 1 }}>
+                                    Google
+                                </Typography>
+                            </Button>
+                        </Grid>
+                    </Grid>
+
+                    <Grid
+                        container
+                        direction='row'
+                        justifyContent='end'>
+                        <Link component={RouterLink} color='inherit' to='/auth/register'>
+                            Crear una cuenta
+                        </Link>
                     </Grid>
                 </form>
             </Grid>
