86f0468a66552583a38c5fca3e3bcabdefcaefbb
Edder Cid <yeyo.foster@gmail.com>
2024-04-19 02:11:05 -0600

    V243 - LoginPage - Diseño sin Layout

--- Seccion 17 - JournalApp - MaterialUI/08-journal-app/src/auth/pages/LoginPage.jsx
+++ Seccion 17 - JournalApp - MaterialUI/08-journal-app/src/auth/pages/LoginPage.jsx
@@ -1,7 +1,46 @@
+import { Grid, TextField, Typography } from "@mui/material"
+
 export const LoginPage = () => {
     return (
-        <>
-            <h1>Login page</h1>
-        </>
+        <Grid
+            container
+            spacing={0}
+            direction='column'
+            alignItems='center'
+            justifyContent='center'
+            sx={{ minHeight: '100vh', backgroundColor: 'primary.main', padding: 4 }}
+        >
+            <Grid item
+                className="box-shadow"
+                xs={3}
+                sx={{ backgroundColor: 'white', padding: 3, borderRadius: 2 }}
+            >
+                <Typography variant="h5" sx={{ mb: 1 }}>
+                    Login
+                </Typography>
+
+                <form>
+                    <Grid container>
+                        <Grid item xs={12} sx={{ mt: 2 }}>
+                            <TextField
+                                label='Correo'
+                                type='email'
+                                placeholder="correo@gmail.com"
+                                fullWidth
+                            />
+                        </Grid>
+
+                        <Grid item xs={12} sx={{ mt: 2 }}>
+                            <TextField
+                                label='Contraseña'
+                                type='password'
+                                placeholder="Contraseña"
+                                fullWidth
+                            />
+                        </Grid>
+                    </Grid>
+                </form>
+            </Grid>
+        </Grid>
     );
 };
