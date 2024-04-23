8161525b0307202ac117358c6b3eaac5813e9c93
Edder Cid <yeyo.foster@gmail.com>
2024-04-23 03:22:29 -0600

    V246 - RegisterPage - Diseño

--- Seccion 17 - JournalApp - MaterialUI/08-journal-app/src/auth/pages/RegisterPage.jsx
+++ Seccion 17 - JournalApp - MaterialUI/08-journal-app/src/auth/pages/RegisterPage.jsx
@@ -1,7 +1,66 @@
+import { Button, Grid, Link, TextField, Typography } from "@mui/material"
+import { Google } from "@mui/icons-material";
+import { Link as RouterLink } from "react-router-dom";
+import { AuthLayout } from "../layout/AuthLayout";
+
 export const RegisterPage = () => {
     return (
-        <>
-            <h1>Register page</h1>
-        </>
+        <AuthLayout title="Crear cuenta">
+            <form>
+                <Grid container>
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField
+                            label='Nombre completo'
+                            type='text'
+                            placeholder="Nombre completo"
+                            fullWidth
+                        />
+                    </Grid>
+
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField
+                            label='Correo'
+                            type='email'
+                            placeholder="correo@gmail.com"
+                            fullWidth
+                        />
+                    </Grid>
+
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField
+                            label='Contraseña'
+                            type='password'
+                            placeholder="Contraseña"
+                            fullWidth
+                        />
+                    </Grid>
+                </Grid>
+
+                <Grid
+                    container
+                    spacing={2}
+                    sx={{ mb: 2, mt: 1 }}>
+                    <Grid item xs={12}>
+                        <Button
+                            variant="contained"
+                            fullWidth>
+                            <Typography>
+                                Login
+                            </Typography>
+                        </Button>
+                    </Grid>
+                </Grid>
+
+                <Grid
+                    container
+                    direction='row'
+                    justifyContent='end'>
+                    <Typography sx={{ mr: 1 }}>¿Ya tienes una cuenta?</Typography>
+                    <Link component={RouterLink} color='inherit' to='/auth/login'>
+                        Ingresar
+                    </Link>
+                </Grid>
+            </form>
+        </AuthLayout>
     );
 };
