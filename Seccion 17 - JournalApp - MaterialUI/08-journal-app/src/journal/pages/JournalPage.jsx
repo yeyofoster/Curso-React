f9f7c15e83d43c8fc862b7d3832867b68838f308
Edder Cid <yeyo.foster@gmail.com>
2024-04-24 02:44:16 -0600

    V247 - JournalLayout y JournalPage

--- Seccion 17 - JournalApp - MaterialUI/08-journal-app/src/journal/pages/JournalPage.jsx
+++ Seccion 17 - JournalApp - MaterialUI/08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,9 +1,13 @@
 import { Typography } from "@mui/material";
+import { JournalLayout } from "../layout/JournalLayout";
 
 export const JournalPage = () => {
     return (
-        <>
-            <Typography variant="h1">Journal page</Typography>
-        </>
+        <JournalLayout>
+            <Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, praesentium exercitationem perferendis laudantium error fuga illo rem quasi consequatur ratione harum atque nobis vero, quod eius aliquam, amet asperiores delectus?</Typography>
+            {/* Nothing selected */}
+
+            {/* NoteView */}
+        </JournalLayout>
     );
 };
