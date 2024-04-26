96d309c44ef41654c4340d5e2f336ecad8fe0df4
Edder Cid <yeyo.foster@gmail.com>
2024-04-26 00:21:00 -0600

    V250 - NothingSelectedView - No hay nada seleccionado

--- Seccion 17 - JournalApp - MaterialUI/08-journal-app/src/journal/layout/JournalLayout.jsx
+++ Seccion 17 - JournalApp - MaterialUI/08-journal-app/src/journal/layout/JournalLayout.jsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Box, Toolbar } from '@mui/material';
 import { NavBar, SideBar } from '../components';
 
 const drawerWidth = 240;
@@ -14,9 +14,9 @@ export const JournalLayout = ({ children }) => {
 
             <Box
                 component='main'
-                sx={{ flexGrow: 1, p: 3 }}
+                sx={{ flexGrow: 1, p: 2 }}
             >
-                {/* Toolbar */}
+                <Toolbar />
                 {children}
             </Box>
         </Box>
--- Seccion 17 - JournalApp - MaterialUI/08-journal-app/src/journal/pages/JournalPage.jsx
+++ Seccion 17 - JournalApp - MaterialUI/08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,11 +1,12 @@
 import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
-            <Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, praesentium exercitationem perferendis laudantium error fuga illo rem quasi consequatur ratione harum atque nobis vero, quod eius aliquam, amet asperiores delectus?</Typography>
-            {/* Nothing selected */}
+            {/* <Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, praesentium exercitationem perferendis laudantium error fuga illo rem quasi consequatur ratione harum atque nobis vero, quod eius aliquam, amet asperiores delectus?</Typography> */}
+            <NothingSelectedView />
 
             {/* NoteView */}
         </JournalLayout>
