5b9fb5750370e43d2c8145e562cb2263e1f64071
Edder Cid <yeyo.foster@gmail.com>
2024-04-26 02:33:44 -0600

    V251 - NoteView

---
+++ Seccion 17 - JournalApp - MaterialUI/08-journal-app/src/journal/components/ImageGallery.jsx
@@ -0,0 +1,5 @@
+export const ImageGallery = () => {
+    return (
+        <div>ImageGallery</div>
+    );
+};
--- Seccion 17 - JournalApp - MaterialUI/08-journal-app/src/journal/pages/JournalPage.jsx
+++ Seccion 17 - JournalApp - MaterialUI/08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,14 +1,15 @@
 import { Typography } from "@mui/material";
 import { JournalLayout } from "../layout/JournalLayout";
-import { NothingSelectedView } from "../views";
+import { NoteView, NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
             {/* <Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, praesentium exercitationem perferendis laudantium error fuga illo rem quasi consequatur ratione harum atque nobis vero, quod eius aliquam, amet asperiores delectus?</Typography> */}
-            <NothingSelectedView />
+            {/* <NothingSelectedView /> */}
 
             {/* NoteView */}
+            <NoteView />
         </JournalLayout>
     );
 };
