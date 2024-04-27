87b42b85715094197ad430d521ae9ea2b4ac2b66
Edder Cid <yeyo.foster@gmail.com>
2024-04-27 01:47:58 -0600

    V253 - Botón flotante

--- Seccion 17 - JournalApp - MaterialUI/08-journal-app/src/journal/pages/JournalPage.jsx
+++ Seccion 17 - JournalApp - MaterialUI/08-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,15 +1,29 @@
-import { Typography } from "@mui/material";
+import { IconButton } from "@mui/material";
+import { AddOutlined } from "@mui/icons-material";
+
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView, NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
-            {/* <Typography>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum, praesentium exercitationem perferendis laudantium error fuga illo rem quasi consequatur ratione harum atque nobis vero, quod eius aliquam, amet asperiores delectus?</Typography> */}
             {/* <NothingSelectedView /> */}
-
-            {/* NoteView */}
             <NoteView />
+
+            <IconButton
+                size="large"
+                sx={{
+                    color: 'white',
+                    backgroundColor: 'error.main',
+                    ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+                    position: 'fixed',
+                    right: 50,
+                    bottom: 50
+                }}
+            >
+                <AddOutlined sx={{ fontSize: 30 }} />
+
+            </IconButton>
         </JournalLayout>
     );
 };
