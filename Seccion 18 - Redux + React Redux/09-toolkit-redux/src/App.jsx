f28cc1c2a291db4e3ef7c904020aced0874a49e5
Edder Cid <yeyo.foster@gmail.com>
2024-05-02 02:47:45 -0600

    V261 - Usar valores del store y despachar acciones

--- Seccion 18 - Redux + React Redux/09-toolkit-redux/src/App.jsx
+++ Seccion 18 - Redux + React Redux/09-toolkit-redux/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment } from './store/slices/counter';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { counter } = useSelector((state) => state.counter);
+  const dispatch = useDispatch();
 
   return (
     <>
@@ -18,8 +20,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => dispatch(increment())}>
+          count is {counter}
         </button>
       </div>
     </>
