f0b7d9599c228c3c3526476eacd61eb215ab4507
Edder Cid <yeyo.foster@gmail.com>
2024-05-05 02:38:09 -0600

    V275 - Configurando Redux en nuestra aplicación

--- Seccion 17 - JournalApp - MaterialUI/08-journal-app/src/main.jsx
+++ Seccion 17 - JournalApp - MaterialUI/08-journal-app/src/main.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import { JournalApp } from './JournalApp'
+import { Provider } from 'react-redux';
+import { store } from './store';
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 );
