ef287c3b01bcb80a82737213970375c1c3e0d3a9
Edder Cid <yeyo.foster@gmail.com>
2024-05-06 02:25:24 -0600

    V279 - Google SignIn - Firebase

--- Seccion 17 - JournalApp - MaterialUI/08-journal-app/src/store/auth/thunks.js
+++ Seccion 17 - JournalApp - MaterialUI/08-journal-app/src/store/auth/thunks.js
@@ -1,3 +1,4 @@
+import { signInWithGoogle } from "../../firebase/providers";
 import { checkingCredentials } from "./authSlice";
 
 export const checkingAuthentication = (email, password) => {
@@ -9,5 +10,7 @@ export const checkingAuthentication = (email, password) => {
 export const startGoogleSignIn = (email, password) => {
     return async (dispatch) => {
         dispatch(checkingCredentials());
+        const result = await signInWithGoogle();
+        console.log({ result });
     };
 };
